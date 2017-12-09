6091c07327f85a92029ace3d6a46d12a2c552dde
Katia <katia.amir@gmail.com>
2017-12-09 11:04:22 -0500

    add an ultra simple conversation bot
    - answers the q:
    'what's your name?'

--- client/app.js
+++ client/app.js
@@ -18,15 +18,14 @@ window.onload = function () {
 
   
   socket.on('chatHistoy', function (history) {
-  	historyString = ''
+	historyString = ''
   	history.forEach(function(el){
   		historyString += el
-  	});
-  	if (historyString.replace(/\s+/g, '') !== document.getElementById('history').innerHTML.replace(/\s+/g, '')) {
-  	   document.getElementById('history').innerHTML += historyString;	
-  	}
-  });
-
+  	  });
+  	  if (historyString.replace(/\s+/g, '') !== document.getElementById('history').innerHTML.replace(/\s+/g, '')) {
+  	    document.getElementById('history').innerHTML += historyString;	
+  	  }
+    });
   socket.on('messageDetails', function (msg) {
   	document.getElementById('history').innerHTML += msg;
   });
--- server.js
+++ server.js
@@ -9,22 +9,37 @@ app.use(express.static('client'));
 var io = require('socket.io')(server);
 var history = [];
 var clientCount = 0
+var conversationSet = {
+  'a': 'Paula', //what's your name? 
+  'b': '26', //how old are you?
+  'c': 'I am a food master', //what do you do? / what do you like?
+  'd': 'I am a sexy woman', //default
+}
 
 io.on('connection', function (socket) {
   io.emit('chatHistoy', history);
-  
   socket.on('messageDetails', function (msg) {
   	var message = msg.text
     var user = msg.name
-    
     var result =
     '<div id="message-block">' +
     '<span><p>' + user + " said: " + message + '</p></span>' + 
     '<span><p>' + new Date().toLocaleTimeString() + '</p></span>' +
     '</div>'
   	var listItem = '<li>' + result + '</li>'
-  	history.push(listItem)
+    history.push(listItem)
     io.emit('messageDetails', listItem);
+    if (message.match(/\?$/) !== null  && 
+    message.match(/^.*?\bwhat\b.*?\bname\b.*?$/m) !== null) {
+      var botResult = 
+      '<div id="message-block">' +
+      '<span><p> Paula said: My name is Paula</p></span>' + 
+      '<span><p>' + new Date().toLocaleTimeString() + '</p></span>' +
+      '</div>' 
+      var listItemBot = '<li>' + botResult + '</li>'
+      history.push(listItemBot)
+      io.emit('messageDetails', listItemBot)
+    }
   });
 });
 
