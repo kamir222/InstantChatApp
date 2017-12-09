148e60c3e3e12dfa3dc01206579e8996731a571b
Katia <katia.amir@gmail.com>
2017-12-09 18:24:56 -0500

    clean up

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ var io = require('socket.io')(server);
 var history = [];
 var clientCount = 0
 var conversationSet = {
-  'a': 'Paula', //what's your name? 
-  'b': '26', //how old are you?
-  'c': 'I am a food master', //what do you do? / what do you like?
-  'd': 'I am a sexy woman', //default
+  'a': 'My name is Paula', //what's your name? 
+  'b': 'I am timeless, baby', //how old are you?
+  'c': 'I am a food wizard, not bragging or anything but im pretty great.', //what do you do? / what do you like?
+  'd': 'Hello. I am Paula. I can only answer these questions:<br/>what is your name?<br/>how old are you?<br/>what do you like?', //default
 }
 
 var botResult = 
@@ -29,58 +29,44 @@ io.on('connection', function (socket) {
   setInterval(function(){ 
     io.emit('messageDetails', listItemBot); 
   }, 20000); //send a scarring message every 20 seconds
+
   socket.on('messageDetails', function (msg) {
   	var message = msg.text
     var user = msg.name
+    var formattedMessage = message.toLowerCase()
+    var msgTimeStamp = '<span><p>' + new Date().toLocaleTimeString() + '</p></span>'
     var result =
-    '<div id="message-block">' +
+    '<div id="message-block">' + '<span><p>' + user + message +
+    '</p></span>' + msgTimeStamp + '</div>'
     // '<span><p>' + user + " said: " + message + '</p></span>' + 
-    '<span><p>' + user + message + '</p></span>' + 
-    '<span><p>' + new Date().toLocaleTimeString() + '</p></span>' +
-    '</div>'
   	var listItem = '<li>' + result + '</li>'
     history.push(listItem)
     io.emit('messageDetails', listItem);
-
-    var formattedMessage = message.toLowerCase()
-
-
     if (formattedMessage.match(/^.*?\bwhat\b.*?\bname\b.*?$/m) !== null) {
       var botResult = 
-      '<div id="message-block">' +
-      '<span><p>My name is Paula</p></span>' + 
-      '<span><p>' + new Date().toLocaleTimeString() + '</p></span>' +
-      '</div>' 
+      '<div id="message-block">' + '<span><p>' + conversationSet.a + 
+      '</p></span>' + msgTimeStamp + '</div>' 
       var listItemBot = '<li>' + botResult + '</li>'
     } else if (formattedMessage.match(/^.*?\what\b.*?\like\b.*?$/m) !== null) {
       var botResult = 
-      '<div id="message-block">' +
-      '<span><p>I am a food wizard, not bragging or anything but im pretty great.</p></span>' + 
-      '<span><p>' + new Date().toLocaleTimeString() + '</p></span>' +
-      '</div>' 
+      '<div id="message-block">' + '<span><p>' + conversationSet.c  + 
+      '</p></span>' + msgTimeStamp + '</div>' 
       var listItemBot = '<li>' + botResult + '</li>'
-    } else if (formattedMessage.match(/^.*?\how\b.*?\old\b.*?$/m) !== null || 
-              formattedMessage.match(/^.*?\what\b.*?\age\b.*?$/m) !== null) {
+    } else if (formattedMessage.match(/^.*?\how\b.*?\old\b.*?$/m) !== null) {
       var botResult = 
-      '<div id="message-block">' +
-      '<span><p>I am timeless, baby</p></span>' + 
-      '<span><p>' + new Date().toLocaleTimeString() + '</p></span>' +
-      '</div>' 
+      '<div id="message-block">' + '<span><p>' + conversationSet.b + 
+      '</p></span>' + msgTimeStamp + '</div>' 
       var listItemBot = '<li>' + botResult + '</li>'
     } else {
       var botResult = 
-      '<div id="message-block">' +
-      '<span><p>Hello. I am Paula. I can only answer these questions:<br/>what is your name?<br/>how old are you?<br/>what do you like?</p></span>' + 
-      '<span><p>' + new Date().toLocaleTimeString() + '</p></span>' +
-      '</div>' 
+      '<div id="message-block">' + '<span><p>' + conversationSet.d + 
+      '</p></span>' + msgTimeStamp + '</div>' 
       var listItemBot = '<li>' + botResult + '</li>'     
     }
     history.push(listItemBot)
     setTimeout(function(){ io.emit('messageDetails', listItemBot); }, 1000);
   });
 });
-
-
 
 server.listen(8080, function() {
   console.log('Chat server running at 8080');
