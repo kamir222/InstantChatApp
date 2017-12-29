ee0224eb186b4b601c682d0f9792caec9aaac1d9
Katia <katia.amir@gmail.com>
2017-12-28 23:06:32 -0500

    convert all tempretures to celsius remove test code

--- server.js
+++ server.js
@@ -22,192 +22,43 @@ const dayForecastExtractor = (forecast, weekDay) => {
 
 const conversationSet = {
   introduction: function(city, region, timeStamp) {
-    return `<div id="message-block"><span><p>
-  Hello I'm Eric, I'm your friendly weather bot. I know the weather in 
-  ${city}, ${region} for the next 7 days.
-  </p></span>${timeStamp}</div>`
+    return `<div id="message-block"><span><p>Hello I'm Eric,
+     I'm your friendly weather bot. I know the weather in 
+     ${city}, ${region} for the next 7 days.</p></span>
+     ${timeStamp}</div>`
   },
   today: function(tempCurrent, conditionsCurrent, timeStamp) {
-    return `<div id="message-block"><span><p>Todays forecast is ${tempCurrent} and it is ${conditionsCurrent}.</p></span>${timeStamp}</div>`
+    return `<div id="message-block"><span><p>Todays forecast is 
+    ${tempCurrent} and it is ${conditionsCurrent}.</p></span>
+    ${timeStamp}</div>`
   },
-  dayOfAWeek: function(day, tempHigh, tempLow) {
-    return `${day} will be a high of ${tempHigh} and a low of ${tempLow}`
+  dayOfAWeek: function(day, tempHigh, tempLow, conditions) {
+    return `${day} will be a high of ${tempHigh} and a low of ${tempLow} and it is ${conditions}`
   },
 }
 
-// const getWeather = callback => {
-//   const request = require('request')
-//   request.get(
-//     `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22toronto%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`,
-//     (error, response) => {
-//       if (!error && response.statusCode == 200) {
-//         const data = JSON.parse(response.body)
-//         callback(data)
-//       }
-//     }
-//   )
-// }
+const getWeather = callback => {
+  const request = require('request')
+  request.get(
+    `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22toronto%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`,
+    (error, response) => {
+      if (!error && response.statusCode == 200) {
+        const data = JSON.parse(response.body)
+        callback(data)
+      }
+    }
+  )
+}
 
 const fahrenheitToCelsiusConverter = number => {
   return Math.round((number - 32) * 0.5556)
 }
 
-// let weatherApi = {}
-// getWeather(weather => {
-//   weatherApi = weather
-// })
-const weatherApi = {
-  query: {
-    count: 1,
-    created: '2017-12-19T20:32:49Z',
-    lang: 'en-GB',
-    results: {
-      channel: {
-        units: {
-          distance: 'mi',
-          pressure: 'in',
-          speed: 'mph',
-          temperature: 'F',
-        },
-        title: 'Yahoo! Weather - Toronto, ON, CA',
-        link:
-          'http://us.rd.yahoo.com/dailynews/rss/weather/Country__Country/*https://weather.yahoo.com/country/state/city-4118/',
-        description: 'Yahoo! Weather for Toronto, ON, CA',
-        language: 'en-us',
-        lastBuildDate: 'Tue, 19 Dec 2017 03:32 PM EST',
-        ttl: '60',
-        location: {
-          city: 'Toronto',
-          country: 'Canada',
-          region: ' ON',
-        },
-        wind: {
-          chill: '36',
-          direction: '255',
-          speed: '32',
-        },
-        atmosphere: {
-          humidity: '66',
-          pressure: '993.0',
-          rising: '0',
-          visibility: '16.1',
-        },
-        astronomy: {
-          sunrise: '7:47 am',
-          sunset: '4:43 pm',
-        },
-        image: {
-          title: 'Yahoo! Weather',
-          width: '142',
-          height: '18',
-          link: 'http://weather.yahoo.com',
-          url: 'http://l.yimg.com/a/i/brand/purplelogo//uh/us/news-wea.gif',
-        },
-        item: {
-          title: 'Conditions for Toronto, ON, CA at 02:00 PM EST',
-          lat: '43.64856',
-          long: '-79.385368',
-          link:
-            'http://us.rd.yahoo.com/dailynews/rss/weather/Country__Country/*https://weather.yahoo.com/country/state/city-4118/',
-          pubDate: 'Tue, 19 Dec 2017 02:00 PM EST',
-          condition: {
-            code: '23',
-            date: 'Tue, 19 Dec 2017 02:00 PM EST',
-            temp: '44',
-            text: 'Breezy',
-          },
-          forecast: [
-            {
-              code: '23',
-              date: '19 Dec 2017',
-              day: 'Tue',
-              high: '44',
-              low: '35',
-              text: 'Breezy',
-            },
-            {
-              code: '30',
-              date: '20 Dec 2017',
-              day: 'Wed',
-              high: '34',
-              low: '23',
-              text: 'Partly Cloudy',
-            },
-            {
-              code: '14',
-              date: '21 Dec 2017',
-              day: 'Thu',
-              high: '26',
-              low: '18',
-              text: 'Snow Showers',
-            },
-            {
-              code: '16',
-              date: '22 Dec 2017',
-              day: 'Fri',
-              high: '29',
-              low: '24',
-              text: 'Snow',
-            },
-            {
-              code: '5',
-              date: '23 Dec 2017',
-              day: 'Sat',
-              high: '44',
-              low: '30',
-              text: 'Rain And Snow',
-            },
-            {
-              code: '28',
-              date: '24 Dec 2017',
-              day: 'Sun',
-              high: '35',
-              low: '26',
-              text: 'Mostly Cloudy',
-            },
-            {
-              code: '16',
-              date: '25 Dec 2017',
-              day: 'Mon',
-              high: '26',
-              low: '18',
-              text: 'Snow',
-            },
-            {
-              code: '23',
-              date: '26 Dec 2017',
-              day: 'Tue',
-              high: '20',
-              low: '7',
-              text: 'Breezy',
-            },
-            {
-              code: '30',
-              date: '27 Dec 2017',
-              day: 'Wed',
-              high: '16',
-              low: '4',
-              text: 'Partly Cloudy',
-            },
-            {
-              code: '30',
-              date: '28 Dec 2017',
-              day: 'Thu',
-              high: '17',
-              low: '6',
-              text: 'Partly Cloudy',
-            },
-          ],
-          description:
-            '<![CDATA[<img src="http://l.yimg.com/a/i/us/we/52/23.gif"/>\n<BR />\n<b>Current Conditions:</b>\n<BR />Breezy\n<BR />\n<BR />\n<b>Forecast:</b>\n<BR /> Tue - Breezy. High: 44Low: 35\n<BR /> Wed - Partly Cloudy. High: 34Low: 23\n<BR /> Thu - Snow Showers. High: 26Low: 18\n<BR /> Fri - Snow. High: 29Low: 24\n<BR /> Sat - Rain And Snow. High: 44Low: 30\n<BR />\n<BR />\n<a href="http://us.rd.yahoo.com/dailynews/rss/weather/Country__Country/*https://weather.yahoo.com/country/state/city-4118/">Full Forecast at Yahoo! Weather</a>\n<BR />\n<BR />\n<BR />\n]]>',
-          guid: {
-            isPermaLink: 'false',
-          },
-        },
-      },
-    },
-  },
-}
+let weatherApi = {}
+getWeather(weather => {
+  weatherApi = weather
+})
+
 const isEmpty = obj => {
   return Object.keys(obj).length === 0
 }
@@ -221,12 +72,12 @@ io.on('connection', function(socket) {
   let location = {}
   let currentConditions = {}
 
-  //if (!isEmpty(weatherApi)) {
-  results = weatherApi['query']['results']['channel']
-  forecast = results['item']['forecast']
-  location = results['location']
-  currentConditions = results['item']['condition']
-  //}
+  if (!isEmpty(weatherApi)) {
+    results = weatherApi['query']['results']['channel']
+    forecast = results['item']['forecast']
+    location = results['location']
+    currentConditions = results['item']['condition']
+  }
 
   console.log(results)
 
@@ -253,55 +104,62 @@ io.on('connection', function(socket) {
         var botWeatherMsg = conversationSet.dayOfAWeek(
           'monday',
           fahrenheitToCelsiusConverter(mondayForecast.high),
-          fahrenheitToCelsiusConverter(mondayForecast.low)
+          fahrenheitToCelsiusConverter(mondayForecast.low),
+          mondayForecast.text
         )
         break
       case formattedMessage.match(/\btuesday\b/) !== null:
         let tuesdayForecast = dayForecastExtractor(forecast, 'Tue')
         var botWeatherMsg = conversationSet.dayOfAWeek(
           'tuesday',
-          tuesdayForecast.high,
-          tuesdayForecast.low
+          fahrenheitToCelsiusConverter(tuesdayForecast.high),
+          fahrenheitToCelsiusConverter(tuesdayForecast.low),
+          tuesdayForecast.text
         )
         break
       case formattedMessage.match(/\bwednesday\b/) !== null:
         let wednesdayForecast = dayForecastExtractor(forecast, 'Wed')
         var botWeatherMsg = conversationSet.dayOfAWeek(
           'wednesday',
-          wednesdayForecast.high,
-          wednesdayForecast.low
+          fahrenheitToCelsiusConverter(wednesdayForecast.high),
+          fahrenheitToCelsiusConverter(wednesdayForecast.low),
+          wednesdayForecast.text
         )
         break
       case formattedMessage.match(/\bthursday\b/) !== null:
         let thursdayForecast = dayForecastExtractor(forecast, 'Thu')
         var botWeatherMsg = conversationSet.dayOfAWeek(
           'thursday',
-          thursdayForecast.high,
-          thursdayForecast.low
+          fahrenheitToCelsiusConverter(thursdayForecast.high),
+          fahrenheitToCelsiusConverter(thursdayForecast.low),
+          thursdayForecast.text
         )
         break
       case formattedMessage.match(/\bfriday\b/) !== null:
         let fridayForecast = dayForecastExtractor(forecast, 'Fri')
         var botWeatherMsg = conversationSet.dayOfAWeek(
           'friday',
-          fridayForecast.high,
-          fridayForecast.low
+          fahrenheitToCelsiusConverter(fridayForecast.high),
+          fahrenheitToCelsiusConverter(fridayForecast.low),
+          fridayForecast.text
         )
         break
       case formattedMessage.match(/\bsaturday\b/) !== null:
         let saturdayForecast = dayForecastExtractor(forecast, 'Sat')
         var botWeatherMsg = conversationSet.dayOfAWeek(
           'saturday',
-          saturdayForecast.high,
-          requestedForecast.low
+          fahrenheitToCelsiusConverter(saturdayForecast.high),
+          fahrenheitToCelsiusConverter(requestedForecast.low),
+          saturdayForecast.text
         )
         break
       case formattedMessage.match(/\bsunday\b/) !== null:
         let sundayForecast = dayForecastExtractor(forecast, 'Sun')
         var botWeatherMsg = conversationSet.dayOfAWeek(
           'sunday',
-          sundayForecast.high,
-          sundayForecast.low
+          fahrenheitToCelsiusConverter(sundayForecast.high),
+          fahrenheitToCelsiusConverter(sundayForecast.low),
+          sundayForecast.text
         )
         break
       default:
